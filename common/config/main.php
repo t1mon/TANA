90ce3f50ce03804b0199c9164f4b136b9746554a
Дмитрий Горин <gorin163@gmail.com>
2020-04-29 09:13:16 +0400

    add set_time_limit

--- common/config/main.php
+++ common/config/main.php
@@ -25,7 +25,7 @@ return [
             'productClass' => \shop\Exchange_1C\Product::class,
             'offerClass' => \shop\Exchange_1C\Offer::class,
             //'on afterProductSync' => [\shop\Exchange_1C\LoadDataBaseShop\ProductLoad::class, 'afterProductSync'],
-            //'on afterUpdateProduct' => [\shop\Exchange_1C\LoadDataBaseShop\ProductLoad::class, 'afterUpdateProduct'],
+            'on afterUpdateProduct' => [\shop\Exchange_1C\LoadDataBaseShop\ProductLoad::class, 'afterUpdateProduct'],
             'on afterOfferSync' => [\shop\Exchange_1C\LoadDataBaseShop\ProductLoad::class, 'afterOfferSync'],
             //'on afterUpdateOffer' => [\shop\Exchange_1C\LoadDataBaseShop\ProductLoad::class, 'afterUpdateOffer'],
             'on afterFinishUploadFile' => [\shop\Exchange_1C\LoadDataBaseShop\ProductLoad::class, 'afterFinishUploadFile'],
