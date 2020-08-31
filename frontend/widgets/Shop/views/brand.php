f706b541c59acbe12156016d123bb51601edfb13
Дмитрий Горин <gorin163@gmail.com>
2020-08-31 21:22:00 +0400

    Исправлена ошибка повторной отправки бренда

--- frontend/widgets/Shop/views/brand.php
+++ frontend/widgets/Shop/views/brand.php
@@ -4,7 +4,7 @@
     <div class="sidebar-widget-tag mt-25">
         <ul>
             <?php foreach ($brands as $brand):?>
-            <li><a href="<?=\yii\helpers\Url::to(['shop/catalog/brand', 'id' => $brand['id']])?>" data-method="POST"><?=$brand['name']?></a></li>
+            <li><a href="<?=\yii\helpers\Url::to(['shop/catalog/brand', 'id' => $brand['id']])?>" data-method="GET"><?=$brand['name']?></a></li>
             <?php endforeach;?>
         </ul>
     </div>
